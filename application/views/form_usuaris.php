81e867dcd7742ca652dd0b54c51775a1529bdb49
jordiromero <jordiromero@iesebre.com>
2015-01-29 22:06:56 +0100

    201501292206

--- application/views/form_usuaris.php
+++ application/views/form_usuaris.php
@@ -1,2 +1,42 @@
+	<style type="text/css">
+			body {
+				padding-top: 40px;
+				padding-bottom: 40px;
+				
+				}
+	</style>
 <body>
+	<div class="container">
+		<form>
+			<div class="form-group">
+				<label for="inputEmail">Correu Electronic:</label>
+				<input type="email" class="form-control" id="inputEmail" placeholder="Inserta correu electronic">
+			</div><!-- fi form-group-->
+			<div class="form-group">
+				<label for="inputNom">Nom:</label>
+				<input type="text" class="form-control" id="inputNom" placeholder="Nom">
+			</div><!-- fi form-group-->
+			<div class="form-group">
+				<label for="inputCognoms">Cognoms:</label>
+				<input type="text" class="form-control" id="inputCognoms" placeholder="Cognoms">
+			</div><!-- fi form-group-->
+			<div class="form-group">
+				<label for="inputUserName">Nom Usuari:</label>
+				<input type="text" class="form-control" id="inputUserName" placeholder="Nom Usuari">
+			</div><!-- fi form-group-->
+			<div class="form-group">
+				<label for="inputPassword">Contrasenya:</label>
+				<input type="password" class="form-control" id="inputPassword" placeholder="Contrasenya">
+			</div><!-- fi form-group-->
+			<div class="form-group">
+				<label for="inputRePassword">Reescriu la contrasenya:</label>
+				<input type="password" class="form-control" id="inputRePassword" placeholder="Reescriu Contrasenya">
+			</div><!-- fi form-group-->
+			<button type="submit" class="btn btn-lg btn-primary btn-block">Enviar</button>
+
+
+		</form><!--fi formulari-->
+
+	</div><!-- fi div container-->
 </body>
+</html>
--- application/views/inici.php
+++ application/views/inici.php
@@ -2,8 +2,45 @@
 	<body>
 		<div class="container">
 			<h1>Hello World P&agrave;gina d'Inici!!!</h1>
+			<table class="table table-bordered">
+  				 <thead>
+        			<tr>
+          				<th>#</th>
+          				<th>First Name</th>
+          				<th>Last Name</th>
+          				<th>Username</th>
+        			</tr>
+      			</thead>
+      			<tbody>
+        			<tr>
+          				<th scope="row">1</th>
+          				<td>Mark</td>
+          				<td>Otto</td>
+          				<td>@mdo</td>
+        			</tr>
+        			<tr>
+          				<th scope="row">2</th>
+          				<td>Jacob</td>
+          				<td>Thornton</td>
+          				<td>@fat</td>
+        			</tr>
+        			<tr>
+          				<th scope="row">3</th>
+          				<td>Larry</td>
+          				<td>the Bird</td>
+          				<td>@twitter</td>
+        			</tr>
+      			</tbody>
+			</table>
 
-		</div>
+		</div><!--fi class container -->
+		<div class="footer">
+			<div class="alert alert-success">
+				<a href="#" class="close" data-dismiss="alert">&times;</a>
+				<strong>Alerta!</strong>Missatge d'alerta.
+
+			</div><!-- fi alert-->
+		</div><!--fi class footer-->
 		
 
 	</body>
