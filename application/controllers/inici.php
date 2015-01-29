a64454a5e610d3ac945bd72b0f7b1a7baeb21f6a
jordiromero <jordiromero@iesebre.com>
2015-01-29 12:21:05 +0100

    20152901

--- application/controllers/inici.php
+++ application/controllers/inici.php
@@ -30,13 +30,15 @@ class Inici extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
+	public function index(){
+
+		$this->load->view('head');
 		$this->load->view('inici');
 	}
 
 	public function login(){
-		
+
+		$this->load->view('head');
 		$this->load->view('login');
 	}
 
@@ -44,6 +46,7 @@ class Inici extends CI_Controller {
 
 	public function _receptes_output($output = null){
 
+		$this->load->view('head');
 		$this->load->view('receptes.php', $output);
 
 	}
@@ -51,6 +54,7 @@ class Inici extends CI_Controller {
 	/* Funció receptes */
 
 	public function receptes(){
+
 		$crud = new grocery_CRUD();
 		$crud->set_table('recepta');
 		$crud->display_as('tipus_recepta_tipus_id', 'Tipus Recepta');
@@ -62,31 +66,15 @@ class Inici extends CI_Controller {
 
 	}
 
-	public function _usuaris_output($output = null){
-
-		$this->load->view('usuaris.php', $output);
-
-	}
-
+	
+	/* Funció Usuaris */
 	public function usuaris(){
-		$crud = new grocery_CRUD();
-		$crud->set_table('usuaris');
-		$crud->columns('nom','cognom','username','password');
-		$crud->fields('nom','cognom','username','password');
-		$crud->required_fields('nom','cognom','username','password');
-		$crud->change_field_type('password', 'password');
-		$crud->callback_insert(array($this,'encrypt_password_and_insert_callback'));
-		$output = $crud->render();
-		$this->_usuaris_output($output);
-
+		$this->load->view('head');
+		$this->load->view('form_usuaris');
+		
 	}
 
-	public function encrypt_password_and_insert_callback($post_array) {
-		$this->load->library('encrypt');
-		$key = 'super-secret-key';
-		$post_array['password'] = $this->encrypt->encode($post_array['password'], $key);
-		return $this->db->insert('usuaris',$post_array);;
-	}        
+	
 
 
 }
---
+++ application/views/head.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap-theme.min.css">
+		<script type="text/javascript" src="<?php echo base_url();?>assets/jquery/jquery-1.11.2.js"></script>
+		<script type="text/javascript" src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
+
+	</head>
